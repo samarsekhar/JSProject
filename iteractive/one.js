16b6a61739922160d949401115ae39932acb9902
samar <samarsekhar25@gmail.com>
2022-01-09 14:44:41 +0530

    commit

--- iteractive/one.js
+++ iteractive/one.js
@@ -1,5 +1,5 @@
 // wap tp print 1 to 10
 
-for (var i = 1; i < 10 ; i + 0) {
+for (var i = 1; i < 10 ; i = i + 1) {
     console.log(i);
 }
---
+++ iteractive/two.js
@@ -0,0 +1,5 @@
+// Wap to print 10 to 1
+
+for (let i = 10; i >= 1; i = i - 1) {
+    console.log(i);
+}
