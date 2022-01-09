bbee337fd03bf9ccab155865e6833d4f96fd4beb
samar <samarsekhar25@gmail.com>
2022-01-09 14:15:44 +0530

    okie

---
+++ NaN test.js
@@ -0,0 +1,5 @@
+console.log(Math.sqrt(-1));
+console.log(30 * "Two");
+console.log("Hello" / "Good Morning");
+console.log(Number("Hello"));
+console.log(typeof NaN);
---
+++ syntax.js
@@ -0,0 +1,11 @@
+switch (n) {
+    case 1:
+        //Execute code block 1
+        break;
+    case 2:
+        //Execute code block 2
+        break;
+
+    default:
+        //code to be executed if n values are different from case 1 and 2 
+}
