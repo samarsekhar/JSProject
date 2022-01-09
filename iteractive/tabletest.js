cd4e9ef5cea48fc1909ece84414d26c7c9e469f6
samar <samarsekhar25@gmail.com>
2022-01-09 14:34:34 +0530

    its try

---
+++ iteractive/tabletest.js
@@ -0,0 +1,8 @@
+let a = 1;
+let i = 1;
+console.log(i)
+
+while (1 <= 5) {
+    console.log( i * 5)
+    i = i + 1
+}
---
+++ iteractive/test.js
@@ -0,0 +1,5 @@
+for (let i = 1; i <=20; i + 1) {
+    var b = 1;
+    console.log(b);
+    b = i * 5
+}
