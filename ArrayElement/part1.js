bd9d9dabe9e0f8fc51af7ea588d08ad6dbb5913f
samar <samarsekhar25@gmail.com>
2022-01-11 22:40:49 +0530

    commit

---
+++ ArrayElement/part1.js
@@ -0,0 +1,10 @@
+var product = [
+    { name: "Pizza", price: "250" },
+    { name: "Chicken65", price: "350"},
+];
+
+// Printing all the Data of Array with Objects
+
+console.log(product);
+
+// Output 
