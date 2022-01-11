6636c676eaec32e8291f1587daafad0a7f11a5b2
samar <samarsekhar25@gmail.com>
2022-01-11 23:07:19 +0530

    commit

---
+++ JSFunction/returnFN.js
@@ -0,0 +1,6 @@
+let x = myFunction(4, 3);       // Function is called, return value will end up in x....
+
+function myFunction(a, b) {       // Function returns the product of a and b.....
+    return a * b;
+}
+console.log(x)  
