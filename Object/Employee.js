d1a155af70eae105bbc5525f8688db6b02df0252
samar <samarsekhar25@gmail.com>
2022-01-12 15:50:40 +0530

    okie

---
+++ Object/Employee.js
@@ -0,0 +1,9 @@
+let emp = {
+    id: 501,
+    name: 'Sam Sahoo',
+    email: 'Sam@tcs.com',
+    loc: 'Bangalore',
+    sal: 20000,
+    pre_loc: ['Odisha', 'India', 'Bangalore']
+}
+console.log(emp);
