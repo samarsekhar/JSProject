4cfb0c0874e7ed53044a2414a206dc7482c692f8
samar <samarsekhar25@gmail.com>
2022-01-12 14:52:30 +0530

    okie

---
+++ Object/ArrayObject.js
@@ -0,0 +1,9 @@
+let employees = [{id: 100, name: "Soheb"}, { id: 101, name: "Sekhar"}, {id: 103}]
+// console.log(employee.length)
+console.log(employees[0].id);
+console.log(employees[0].name);
+console.log(employees[0].id,employees[1].name);
+
+let x = 1000
+let y = 2000
+console.log(x,y);
