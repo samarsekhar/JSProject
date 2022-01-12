7cb7e5cf5073c4498098416aee4edd3eaad21d6f
samar <samarsekhar25@gmail.com>
2022-01-12 15:00:59 +0530

    ok

---
+++ Object/ArrayObject2.js
@@ -0,0 +1,11 @@
+let employees = [{id: 501, name:'Prabir'}, {id: 502}, {id: 503}]
+
+console.log(employees[1].id);
+console.log(employees[1].name);
+console.log(employees.length);
+
+let a = 1234
+let b = 1235
+console.log(a, b);
+console.log(a + b);
+console.log(a * b);
