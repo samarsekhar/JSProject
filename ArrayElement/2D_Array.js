f81d18bd674ad678336e25cb76cd7872e3dd7caa
samar <samarsekhar25@gmail.com>
2022-01-12 22:05:21 +0530

    okie

---
+++ ArrayElement/2D_Array.js
@@ -0,0 +1,11 @@
+var array2d = [
+    [1, 2, 4],
+    [2, 3, 5],
+    [6, 2, 8],
+]
+console.log(array2d[1]);
+console.log(array2d[2]);
+console.log(array2d[1][2]);
+
+array2d[0][2] = 40;
+console.log(array2d);
---
+++ ForLoop/index1.js
@@ -0,0 +1,4 @@
+var numbers = [100, 200, 300, 400];
+for(var i=0 ; i <= numbers.length ; i++) {
+    console.log(numbers[i]);
+}
