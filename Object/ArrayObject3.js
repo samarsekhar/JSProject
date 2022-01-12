23e1ca3d11e6705a4e8f0ee4911ca35067364b80
samar <samarsekhar25@gmail.com>
2022-01-12 15:09:13 +0530

    okie

---
+++ Object/ArrayObject3.js
@@ -0,0 +1,7 @@
+let players = [{id: 18, name: 'Virat'}, {id: 7, name: 'Dhoni'}, {id: 3}]
+
+console.log(players.length);
+console.log(players[2].id);
+console.log(players[1].name);
+console.log(players[0].name);
+console.log(players[1].name);
