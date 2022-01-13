95c6a0a8e355bfb3c5e4efac834e4536c3805a3e
samar <samarsekhar25@gmail.com>
2022-01-13 20:57:55 +0530

    its okey

---
+++ fatArrow/seven.js
@@ -0,0 +1,3 @@
+let multi = (a, b, c) => a * b * c + 120;
+let result = multi (40, 30, 40);
+console.log(result);
---
+++ fatArrow/six.js
@@ -0,0 +1,7 @@
+/*let add = (x, y) => {return x + y}
+let x = add(10, 30 + 100);
+console.log(x);*/
+
+let add = (x, y) => x + y
+let a = add(10 , 40);
+console.log(a);
