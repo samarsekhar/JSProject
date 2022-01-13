b6c97d5a2a23105bdbcbb72d20431c18f2ea5c01
samar <samarsekhar25@gmail.com>
2022-01-13 20:47:14 +0530

    okey

---
+++ fatArrow/five.js
@@ -0,0 +1,11 @@
+/*let greet = () => {
+    return "Good Morning JS"
+}
+let message = greet();
+console.log(message);*/
+
+// or//
+
+let greet = () => "Good Morning Python"
+
+console.log(greet());
