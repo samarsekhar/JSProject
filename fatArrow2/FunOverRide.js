fb87a56d483cfdf3a40cbd4dbd0ecf4845219e70
samar <samarsekhar25@gmail.com>
2022-01-14 16:34:09 +0530

    okie

---
+++ fatArrow2/FunOverRide.js
@@ -0,0 +1,16 @@
+/*function wish() {
+    console.log("Good Morning Python");
+}
+wish()
+function wish () {console.log("Good morning Python");}
+wish()*/
+
+var a = 100
+var wish = () => {
+    console.log("Good Morning Python");
+}
+wish()
+var wish = () => {
+    console.log("Good Morning JS");
+}
+wish()
