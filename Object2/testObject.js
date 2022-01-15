40f4634b8e097b8039753f6dfdd4cd2860737e91
samar <samarsekhar25@gmail.com>
2022-01-15 17:20:09 +0530

    okie

---
+++ Object2/testObject.js
@@ -0,0 +1,11 @@
+let person = {
+    firstName : "Sekhar",
+    lastName : "Panigrahi",
+    age : 23,
+    eyeColor : "blue",
+    get_person_Details : () => {return}
+}
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
+console.log(person.get_person_Details());
