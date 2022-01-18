10d748919204339f7fa4836ecf84840c8a64c778
samar <samarsekhar25@gmail.com>
2022-01-18 19:34:56 +0530

    okie

---
+++ Class/part1.js
@@ -0,0 +1,10 @@
+class office {
+    constructor() {
+        console.log("Emplpyee One");
+    }
+    open_office() {
+        console.log("Employee Two");
+    }
+}
+let c1 = new office();
+c1.open_office()
