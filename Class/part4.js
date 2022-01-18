06d3acd5f194dd5732c96e36f9cb0dc1ae06f8b5
samar <samarsekhar25@gmail.com>
2022-01-18 22:01:31 +0530

    okie

---
+++ Class/part4.js
@@ -0,0 +1,10 @@
+class Employee {
+    constructor(name, sal, loc) {
+        this.emp_name = name;
+        this.emp_sal = sal;
+        this.emp_loc = loc;
+        console.log();
+    }
+}
+let e1 = new Employee("SamarSekhar", 24000, "Bangalore");
+console.log(e1);
