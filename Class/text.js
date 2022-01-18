9560813237ab3369307a46e727bf90ffd8072d08
samar <samarsekhar25@gmail.com>
2022-01-18 20:01:10 +0530

    I did this

--- Class/text.js
+++ Class/text.js
@@ -6,8 +6,9 @@ class office {
         this.emp_Name = (b);
     }
     open_office() {
-        console.log(`${this.emp_Name} - You are qualified the interview successfully and emp_id: ${this.emp_id}`);
+        console.log(this.emp_Name, "You are qualified the interview successfully", this.emp_id);
     }
 }
-let c1 = new office(1010, 'Sam');
+let c1 = new office(12, 'Sekhar');
+console.log(c1);
 c1.open_office()
