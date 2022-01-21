b58a27b4fcd8ef2887875bb2b83471d3f52a3ffa
samar <samarsekhar25@gmail.com>
2022-01-21 19:50:02 +0530

    trying

---
+++ A_A_revision/DOM/one.js
@@ -0,0 +1,9 @@
+function getDate() {
+    let a = document.getElementById('sam');
+    a.innerHTML = "Hello World"
+
+    document.getElementById('Alpha').innerHTML = "I Do Everything "
+
+    let name = document.getElementById('sek').innerHTML;
+    console.log(name);
+}   
