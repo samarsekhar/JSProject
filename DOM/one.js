e0bc31b4c49e69b475e4a91a8e76787b722de9d5
samar <samarsekhar25@gmail.com>
2022-01-21 19:29:35 +0530

    okie

---
+++ DOM/one.js
@@ -0,0 +1,9 @@
+function getData() {
+    let x = document.getElementById('abc');
+    x.innerHTML = "Hello, Good Afternoon"
+
+    let name = document.getElementById('xyz');
+    console.log(name);
+
+    document.getElementById('Alpha').innerHTML = "I Love Programming"
+}
