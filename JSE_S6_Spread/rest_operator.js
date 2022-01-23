9339ce18c7ff217c966024e08bf7bd0cf58cb268
samar <samarsekhar25@gmail.com>
2022-01-23 20:19:50 +0530

    okie

---
+++ JSE_S6_Spread/rest_operator.js
@@ -0,0 +1,6 @@
+function add(a, ...b) {
+    console.log(a);
+    console.log(b);
+    console.log(a, b);
+}
+add(1, 2)
---
+++ JSE_S6_Spread/spread_for_loop/ex1.js
@@ -0,0 +1,7 @@
+let marks = [55, 66, 77, 88, 99]
+let b = []
+
+for (let i = 0; i <= marks.length - 1; i++) {
+    b.push(marks[i])
+}
+console.log(b);
