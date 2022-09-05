77c0e3377f0ed293ae9cbd2dd924c2a2303826cc
samar <samarsekhar25@gmail.com>
2022-09-05 22:10:05 +0530

    add three numbres

---
+++ JS_Sep/01_Introduction.js
@@ -0,0 +1,12 @@
+console.log("This program adds 1, 2, and 43");
+
+function addThreeNumbers(a, b, c) {
+    return a + b + c;
+}
+let c = addThreeNumbers(1, 2, 43);
+
+console.log(c);
+
+
+
+// output:- 46  
