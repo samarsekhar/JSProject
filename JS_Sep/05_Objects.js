7307c2d4e252765726108157067415aaa2ee07a1
samar <samarsekhar25@gmail.com>
2022-09-06 13:24:55 +0530

    JS Non-Primitive DataType like Object

---
+++ JS_Sep/05_Objects.js
@@ -0,0 +1,13 @@
+// Non-Primitive DataType JS - Objects in JS
+
+const item = {
+    "Pooja" : true,
+    "Kathirine" : false,
+    "Sekhar" : 56,
+    "Devil" : undefined,
+}
+
+console.log(item.Pooja);
+console.log(item.Kathirine);
+console.log(item["Sekhar"]);   // Both are same
+console.log(item["Devil"]);
