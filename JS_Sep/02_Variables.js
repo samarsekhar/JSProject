91daf3c58714d4fd0ab5be5cbfb61d059f899866
samar <samarsekhar25@gmail.com>
2022-09-06 12:54:24 +0530

    JavaScript Variables....

---
+++ JS_Sep/02_Variables.js
@@ -0,0 +1,11 @@
+console.log("Variables");
+
+var a = 67  // a contains 67
+console.log(a); 
+
+a = "Pooja"
+console.log(a);
+
+// let 8Pooja = 45 // Not allowed this will throw an error
+// let var = 7     // Not allowed
+
