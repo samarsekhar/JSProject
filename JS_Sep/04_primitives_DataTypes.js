940997527413c4cf8808e6144ebd9e6d4c6b1087
samar <samarsekhar25@gmail.com>
2022-09-06 13:18:04 +0530

    JS Primitives 7 DataTypes example.

---
+++ JS_Sep/04_primitives_DataTypes.js
@@ -0,0 +1,23 @@
+// Primitives DataTypes JS (SSBBNNU)
+// String, Symbol, Boolean, BigInt, Null, Number, undefined
+
+let a = "Pooja";
+let b = Symbol("id");
+let c = true // can also be false
+let d = BigInt("12316652171");
+let e = BigInt("12345") * BigInt("5");
+let f = null
+let g = 43566;
+let h = undefined;
+let i 
+
+console.log(a, b, c, d, e, f, g, h, i);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof f);
+console.log(typeof g);
+console.log(typeof h);
+console.log(typeof i);
+
