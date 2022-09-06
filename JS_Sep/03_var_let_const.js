28481f3a61b61c471c0b419aed18358449062e42
samar <samarsekhar25@gmail.com>
2022-09-06 13:03:39 +0530

    JavaScript var , let and const

---
+++ JS_Sep/03_var_let_const.js
@@ -0,0 +1,20 @@
+console.log("var_let_const");
+
+var a = 45;            // declared Variables
+var b = "Pooja";
+const author = "Sam";
+c = 4;
+const Sam = 07;        // constant indentifiers
+let d = null
+let e = undefined
+
+{
+    let b = "this";     // let is block level
+    console.log(b);
+}
+
+console.log(b);
+console.log(a);
+console.log(d);
+console.log(e);
+console.log(author);
