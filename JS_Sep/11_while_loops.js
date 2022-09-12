960e2a0dcf6e8fb8ce9b95df71d28c725245a829
samar <samarsekhar25@gmail.com>
2022-09-12 17:51:19 +0530

    JS while loop example....

---
+++ JS_Sep/11_while_loops.js
@@ -0,0 +1,37 @@
+// While loop 
+
+/* let n = prompt("Enter the value of n");
+n = Number.parseInt(n)
+
+let i = 0;
+while(i<n) {
+    console.log(i)
+    i++;
+}    */
+
+let n = 43;
+let i = 0;
+while(i<n) {
+    console.log(i)
+    i++;
+}                        // output - 1 to 43
+
+
+// Do while loop
+
+/*let x = prompt("Enter the value of n");
+x = Number.parseInt(x)
+
+let j = 10;
+do {
+    console.log(j)
+    j++
+}while (i < x)
+*/
+
+let x = 43;
+let j = 0;
+do {
+    console.log(j)
+    j++
+}while ( j < x )
