ee35538fac6ca2b4cb1da118495134249e18e5f4
samar <samarsekhar25@gmail.com>
2022-09-15 13:09:40 +0530

    JS Strings methods

---
+++ JS_Sep/14_Strings.js
@@ -0,0 +1,23 @@
+// Strings
+let person = "Pooja";
+console.log(person.length);       // 5
+console.log(person[0]);           // P
+console.log(person[1]);           // o
+
+let friend = "Anissa";
+console.log(friend);       // Anissa
+
+
+// Template literals
+let boy1 = "Shivaa";
+let boy2 = "Ram";
+let sentence = `${boy2} is a frined of ${boy1}.`
+console.log(sentence);          // output- Ram is a friend of Shivaa.
+
+
+// Escape Sequence Characters 
+let fruit = 'Bana\"na'
+console.log(fruit);        // output - Bana"na
+let fruits = "Bana\"na"
+console.log(fruits);       // Both are same output
+
