8ea881c41c2cb7e70feb8bccb30faef33fb594b4
samar <samarsekhar25@gmail.com>
2022-10-31 20:43:43 +0530

    JavaScript console object methods...

---
+++ JS_Oct_Html/25_Console_Object_Methods/script.js
@@ -0,0 +1,40 @@
+console.log("log");
+console.info("info");
+console.warn("warn");
+console.error("err");
+console.assert("err" != false);
+console.assert("err" == false);
+
+console.time("forLoop");
+
+for (let i=0; i<10; i++) {
+    console.log(56)
+}
+
+console.timeEnd("forLoop");
+
+console.time("whileLoop");
+
+let i=0;
+while (i<10) {
+    console.log(43)
+    i++;
+}
+console.timeEnd("whileLoop");
+
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+
+const transportation = data.reduce((counters, item) => {
+  if (!counters[item]) counters[item] = 0;
+  counters[item]++;
+  return counters;
+}, {});
+
+console.log('transportation table');
+console.table(transportation);
+
+console.log('transportation log');
+console.log(transportation);
+
+
+console.table(['apples','oranges','bana']);
