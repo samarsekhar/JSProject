9868f708d60facee965295b9a13ea0d21e89774e
samar <samarsekhar25@gmail.com>
2023-12-30 23:02:23 +0530

    Array Method & CSS Practices

---
+++ #Samar/JSQue/ArrayMethod.js
@@ -0,0 +1,15 @@
+//* Random Array Element
+// let arr = ["Navin", "Pravin", "Sud"];
+// let randomIndex = Math.round(Math.random() * 100) % arr.length;
+
+// console.log(arr[randomIndex]);
+
+//* Create & Fill Array
+// let arr = new Array(4).fill("*");
+
+// console.log(arr);
+
+//* Array to Object
+let array = ["Samar", "Minu"];
+let ob = { ...array };
+console.log(ob);
